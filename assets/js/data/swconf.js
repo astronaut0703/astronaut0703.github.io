96a0209363b2a100870996711a21a57d6f4d3267
seoah <astronaut0703@gmail.com>
2025-07-16 03:04:35 +0900

    fix: add avatar image

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752600497',resources: [
+    cacheName: 'chirpy-1752602651',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
